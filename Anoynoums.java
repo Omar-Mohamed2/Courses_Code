bf1bcfa0433f3b3198177ce1d0266e7b55abc202
Omar Mohamed <122606895+Omar-Mohamed2@users.noreply.github.com>
2023-02-20 11:18:26 +0200

    Add files via upload

---
+++ Anoynoums.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package anoynoums;
+
+/**
+ *
+ * @author Softlaptop
+ */
+public class Anoynoums {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//       c1 c=new c1();
+//                        c1.c2 inner= c.new c2();
+//                inner.disp();
+    ////               c.show();
+    //     c1 c=new c1()
+    //{
+    //            public void show()
+    //{
+    //   System.out.println("world");
+    //} 
+    //}; c.show();
+    //    }
+     
+    new c1()
+{
+            public void show()
+{
+   System.out.println("hello world");
+}
+} .show();
+}
+}
